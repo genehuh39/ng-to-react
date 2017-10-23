f37549958faafdc323b36537a39eda8b3a848176
Gene Huh <ehuh@teamexos.com>
2017-10-22 17:06:36 -0700

    Refactored ItemButtonBar props

--- client/src/components/ItemGridRow.js
+++ client/src/components/ItemGridRow.js
@@ -3,13 +3,18 @@ import ItemButtonBar from './ItemButtonBar';
 
 const ItemGridRow = (props) => {
     return (
-        <tr key={props.id}>
+        <tr key={props.fighter.id}>
             <td>{props.fighter.firstName}</td>
             <td>{props.fighter.lastName}</td>
             <td>{props.fighter.weight}</td>
             <td>{props.fighter.description}</td>
             <td />
-            <td><ItemButtonBar {...props} /></td>
+            <td>
+                <ItemButtonBar
+                    id={props.fighter.id}
+                    selectFighter={props.selectFighter}
+                    deleteFighter={props.deleteFighter} />
+            </td>
         </tr>
     )
 };
