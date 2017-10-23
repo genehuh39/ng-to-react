3ffd7d42d8e2813c9198ddaa05ca490aac674de2
Gene Huh <ehuh@teamexos.com>
2017-10-23 14:35:55 -0700

    Add ItemGridAddButton component

---
+++ client/src/components/ItemGridAddButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ItemGridAddButton = () => {
+    return (
+        <tr>
+            <td colSpan="6">
+                <button type="button" className="btn btn-primary">
+                    add new fighter
+                </button>
+            </td>
+        </tr>
+    );
+};
+
+export default ItemGridAddButton;
--- client/src/components/ItemGridHead.js
+++ client/src/components/ItemGridHead.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import ItemGridAddButton from './ItemGridAddButton';
 
 const ItemGridHead = () => {
     return (
         <thead>
-            <tr>
-                <td colSpan="6">
-                    <button type="button" className="btn btn-primary">
-                        add new fighter
-                    </button>
-                </td>
-            </tr>
+            <ItemGridAddButton />
             <tr>
                 <th>First Name</th>
                 <th>Last Name</th>
