314f0c5a170394edd79d18a7abab6a4e0f6412c8
Gene Huh <ehuh@teamexos.com>
2018-01-19 22:22:24 -0800

    add WeightClass model

---
+++ model/weightClass.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var WeightClass = mongoose.model('WeightClass', {
+    className: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    classCode: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    weight: {
+        type: Number,
+        required: true
+    }
+});
+
+module exports  = { WeightClass }
